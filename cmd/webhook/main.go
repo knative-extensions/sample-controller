05c0efe4890c6e22a974a9c57fb9a6c9ee68c3a3
Nima Kaviani <nkavian@us.ibm.com>
2019-11-14 08:45:07 -0800

    add config validation webhook to sample-controller

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -24,10 +24,13 @@ import (
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/logging"
+	"knative.dev/pkg/metrics"
 	"knative.dev/pkg/signals"
 	"knative.dev/pkg/system"
 	"knative.dev/pkg/webhook"
 	"knative.dev/pkg/webhook/certificates"
+	"knative.dev/pkg/webhook/configmaps"
 	"knative.dev/pkg/webhook/resourcesemantics"
 
 	"knative.dev/sample-controller/pkg/apis/samples/v1alpha1"
@@ -60,8 +63,24 @@ func NewResourceAdmissionController(ctx context.Context, cmw configmap.Watcher) 
 	)
 }
 
+func NewConfigValidationController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	return configmaps.NewAdmissionController(ctx,
+
+		// Name of the configmap webhook.
+		fmt.Sprintf("config.webhook.%s.knative.dev", system.Namespace()),
+
+		// The path on which to serve the webhook.
+		"/config-validation",
+
+		// The configmaps to validate.
+		configmap.Constructors{
+			logging.ConfigMapName(): logging.NewConfigFromConfigMap,
+			metrics.ConfigMapName(): metrics.NewObservabilityConfigFromConfigMap,
+		},
+	)
+}
+
 func main() {
-	// Set up a signal context with our webhook options
 	ctx := webhook.WithOptions(signals.NewContext(), webhook.Options{
 		ServiceName: "webhook",
 		Port:        8443,
@@ -71,5 +90,6 @@ func main() {
 	sharedmain.MainWithContext(ctx, "webhook",
 		certificates.NewController,
 		NewResourceAdmissionController,
+		NewConfigValidationController,
 	)
 }
