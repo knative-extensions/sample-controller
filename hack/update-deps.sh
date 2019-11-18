4d988538d27d72c76df4639145857efbebd9554c
Alan Conway <aconway@redhat.com>
2019-11-18 16:38:25 -0500

    Add set-span-id.patch hack to update-deps.sh
    Signed-off-by: Alan Conway <aconway@redhat.com>

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -28,3 +28,10 @@ dep ensure
 
 rm -rf $(find vendor/ -name 'OWNERS')
 rm -rf $(find vendor/ -name '*_test.go')
+
+# HACK HACK HACK
+# The only way we found to create a consistent Trace tree without any missing Spans is to
+# artificially set the SpanId. See pkg/tracing/traceparent.go for more details.
+# Produced with:
+# git diff origin/master HEAD -- vendor/go.opencensus.io/trace/trace.go > ./hack/set-span-id.patch
+git apply ${REPO_ROOT_DIR}/hack/set-span-id.patch
