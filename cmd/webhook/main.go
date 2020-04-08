17c8a75614d6f9548ee67f625f67eb92b2813aa7
Weston Haught <whaught@google.com>
2020-04-08 10:16:57 -0700

    Include extra validations in example

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -43,6 +43,8 @@ var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{
 	v1alpha1.SchemeGroupVersion.WithKind("AddressableService"): &v1alpha1.AddressableService{},
 }
 
+var callbacks = map[schema.GroupVersionKind]validation.Callback{}
+
 func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
 	return defaulting.NewAdmissionController(ctx,
 
@@ -52,7 +54,7 @@ func NewDefaultingAdmissionController(ctx context.Context, cmw configmap.Watcher
 		// The path on which to serve the webhook.
 		"/defaulting",
 
-		// The resources to validate and default.
+		// The resources to default.
 		types,
 
 		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
@@ -76,7 +78,7 @@ func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher
 		// The path on which to serve the webhook.
 		"/resource-validation",
 
-		// The resources to validate and default.
+		// The resources to validate.
 		types,
 
 		// A function that infuses the context passed to Validate/SetDefaults with custom metadata.
@@ -88,6 +90,9 @@ func NewValidationAdmissionController(ctx context.Context, cmw configmap.Watcher
 
 		// Whether to disallow unknown fields.
 		true,
+
+		// Extra validating callbacks to be applied to resources.
+		callbacks,
 	)
 }
 
