efb555ed1be646502693ffa763c7b40003843972
Scott Nichols <snichols@vmware.com>
2020-10-09 14:21:21 -0700

    leverage the test-infra go_update_deps

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -14,50 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-readonly ROOT_DIR=$(dirname $0)/..
-source ${ROOT_DIR}/vendor/knative.dev/test-infra/scripts/library.sh
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-cd ${ROOT_DIR}
+source $(dirname "$0")/../vendor/knative.dev/test-infra/scripts/library.sh
 
-# Parse flags to determine any we should pass to dep.
-# We need these flags for things to work properly.
-export GO111MODULE=on
-export GOFLAGS=-mod=vendor
-
-# This controls the release branch we track.
-VERSION="master"
-
-# The list of dependencies that we track at HEAD and periodically
-# float forward in this repository.
-FLOATING_DEPS=(
-  "knative.dev/pkg@${VERSION}"
-  "knative.dev/test-infra@${VERSION}"
-)
-
-GO_GET=0
-while [[ $# -ne 0 ]]; do
-  parameter=$1
-  case ${parameter} in
-    --upgrade) GO_GET=1 ;;
-    *) abort "unknown option ${parameter}" ;;
-  esac
-  shift
-done
-readonly GO_GET
-
-if (( GO_GET )); then
-    go get -d ${FLOATING_DEPS[@]}
-fi
-
-# Prune modules.
-go mod tidy
-go mod vendor
-
-rm -rf $(find vendor/ -name 'OWNERS')
-rm -rf $(find vendor/ -name '*_test.go')
-
-update_licenses third_party/VENDOR-LICENSE "./..."
+go_update_deps "$@"
