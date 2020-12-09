3cec4af202da61a550d1705c7050d16dddb1233c
Markus Thömmes <markusthoemmes@me.com>
2020-12-09 17:04:23 +0100

    Bootstrap reconciler

---
+++ pkg/reconciler/simpledeployment/simpledeployment.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package simpledeployment
+
+import (
+	"context"
+
+	"k8s.io/client-go/kubernetes"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+	"knative.dev/pkg/reconciler"
+	samplesv1alpha1 "knative.dev/sample-controller/pkg/apis/samples/v1alpha1"
+	simpledeploymentreconciler "knative.dev/sample-controller/pkg/client/injection/reconciler/samples/v1alpha1/simpledeployment"
+)
+
+// Reconciler implements simpledeploymentreconciler.Interface for
+// SimpleDeployment resources.
+type Reconciler struct {
+	kubeclient kubernetes.Interface
+	podLister  corev1listers.PodLister
+}
+
+// Check that our Reconciler implements Interface
+var _ simpledeploymentreconciler.Interface = (*Reconciler)(nil)
+
+// ReconcileKind implements Interface.ReconcileKind.
+func (r *Reconciler) ReconcileKind(ctx context.Context, d *samplesv1alpha1.SimpleDeployment) reconciler.Event {
+	return nil
+}
